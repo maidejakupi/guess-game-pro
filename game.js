a2ac1335439dad13f4ff49b0d14c95f500c97b02
Maide <mj32808@seeu.edu.mk>
2026-06-18 22:06:30 +0200

    Add game logic

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log("Guess the Number Game") 
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("🎮 Guess the Number Game!");
+console.log("Guess a number between 1 and 100");
+
+let guess = 50; // we will improve this later
+
+if (guess === secretNumber) {
+    console.log("🎉 Correct! You win!");
+} else if (guess > secretNumber) {
+    console.log("📉 Too high!");
+} else {
+    console.log("📈 Too low!");
+}
+
+console.log("Secret was:", secretNumber);
